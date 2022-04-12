fc41eb3b0c7041ec5c3747b574263bdf1978c2b3
ZinhleMavimbela-TomTom <99164103+ZinhleMavimbela-TomTom@users.noreply.github.com>
2022-04-12 09:07:45 +0800

    Update push_html_data.py

--- push_html_data.py
+++ push_html_data.py
@@ -1,1 +1,183 @@
+from bs4 import BeautifulSoup
+import re
+import csv
+import os
+import mysql.connector 
+import fnmatch
+from cgitb import html
+from atlassian import Confluence
+import mysql.connector
+import json
 
+region =str(os.getenv("regions"))
+version =str(os.getenv("version"))
+
+
+csvfilepath = 'country_names.csv'
+# local container for country name and description
+country_isocode_description = []
+
+# list to store unmatched country codes
+unmatched_countries = []
+
+# class CountryData is to hold the name, the ISO code, and the description of each country found in the HTML file
+class CountryData:
+    def __init__(self, name, data_ver, code, description):
+        self.name = name
+        self.data_ver = data_ver
+        self.code = code
+        self.description = description
+
+    # printing helper
+    def __str__(self):
+        return "country name: %s, country ISO code: %s, \ndescription: %s" % (self.data_ver, self.name, self.code, self.description)
+def region_convert():
+
+    if region == "cas":
+        return "Central Asia"
+    if region == "eur":
+        return "Europe"
+    if region == "nam":
+        return "North America"
+    if region == "oce":
+        return "Oceania"
+    if region == "sea":
+        return "Southeast Asia"
+    if region == "mea":
+        return "Middle East Africa"
+    if region == "isr":
+        return "Israel"
+    if region == "lam":
+        return "Latin America"
+    if region == "s_o":
+        return "Pacific Ocean"
+    if region == "ind":
+        return "India"
+    if region == "kor":
+        return "Korea"
+
+def storage_con(data_ver, c_code, descript):
+    #region change to fullname
+    
+    # connection to confluence credentials
+    confluence = Confluence(
+    url = 'https://confluence.tomtomgroup.com/',
+    username='mavimbel',
+    password = 'mavimbela1992'
+    )
+    htmlstring = ""
+    status=confluence.append_page(
+    page_id = confluence.get_page_id(space="MTC", title="1090 Release Notes (2022.03)"),
+    title = "1090 Release Notes (2022.03)", 
+    append_body =htmlstring +"<h5>{}</h5>{}".format(c_code,descript)
+   
+    )
+    print(status)
+
+# function to match the country name with the ISO code from the country_names csv file
+def matching_country_code(name):
+    with open(csvfilepath, 'r') as csvfile:
+        datareader = csv.reader(csvfile)
+        next(datareader)
+        for row in datareader:
+            if row[1] == name:
+                return row[0]
+            else:
+                if re.search(row[1], name):
+                    return row[0]
+
+# main function to read the HTML file and parse through and extract the country name and the country description
+def pulling_data():
+    file_list = f'/home/mavimbel/parse_highlight/highlights/lastest'
+    pattern = f'highlights_and_improvements_mn_{region}_{version}.html'
+    
+    with open(os.path.join(file_list, pattern), 'r') as html_file:
+            content = html_file.read()
+            soup = BeautifulSoup(content, 'html.parser')
+            # get the data source version from the html title
+            title_string = (soup.find('title'))
+            data_source_version = ((title_string.text).split()[2])
+
+            # country names have the h2 tag in the HTML file
+            country_name_tags = soup.find_all('h2')
+            country_names = []
+            # remove extra spaces from the country names
+            for c in country_name_tags:
+                sanitize = re.sub(r"[\n\t]*", "", c.text)
+                sanitize = re.sub(' +', " ", sanitize)
+                country_names.append(sanitize)
+
+            # descriptions have the ul tag in the HTML file
+            description_tags = soup.find_all('ul')
+
+            descriptions = []
+            for d in description_tags:
+                result = d.find_all("li")
+                country_descrip = []
+                for li in result:
+                    # fix random newlines, tabs, spaces in strings
+                    sanitize = u''.join(li.findAll(text=True))
+                    sanitize = re.sub(r"[\n\t]*", "", sanitize)
+                    sanitize = re.sub(' +', " ", sanitize)
+                    sanitize =re.sub('\xa0',"",sanitize)
+                    sanitize = sanitize.strip()
+                    country_descrip.append(sanitize)
+                descriptions.append(country_descrip)
+                del country_descrip
+
+            # remove introduction and general from both country names and the descriptions
+            del country_names[:2]
+            del descriptions[:2]
+
+            # find the ISO codes based on the country name
+            iso_codes = []
+            for country_name in country_names:
+                iso = matching_country_code(country_name)
+                iso_codes.append(iso)
+                if iso is None:
+                    unmatched_countries.append(country_name)
+
+             # order matters, so first error checking is to make sure there is a 1:1:1 correlation between all individual lists
+            if(len(country_names) == len(iso_codes) == len(descriptions)):
+                for i in range(len(country_names)):
+                    # combine all information into the class
+                    country_isocode_description.append(CountryData(
+                        country_names[i], data_source_version, iso_codes[i], descriptions[i]))
+            else:
+                print("Error sizes do not match")
+    
+def print_all():
+    # print to check
+    print("____________________________________________________________________________________________________________________________________")
+    for entry in country_isocode_description:
+        print("__________________________________________________________________________________________________________________________________")
+        print(entry.name, "(", entry.code, ")-", entry.data_ver, "\n", entry.description)
+
+def pushing_data():
+    count=0
+    for country in country_isocode_description:
+        one_country_description_as_string = ""
+        
+        for single in country.description:
+            single="<li>"+ single+ "</li>"
+            one_country_description_as_string = one_country_description_as_string + single
+            one_country_description_as_string = one_country_description_as_string+ "\n"
+        if count==0:
+            assign_region=region_convert()
+            country.name ="<h3>"+assign_region+"</h3><br></br>"+"<h5>"+country.name+"</h5>"
+        storage_con(country.data_ver, country.name, one_country_description_as_string)
+        count=count+1
+        one_country_description_as_string = ""
+
+pulling_data()
+print("Total Country Count: ", len(country_isocode_description))
+
+if (len(unmatched_countries) == 0):
+    print("Pushing to Database")
+    #pushing_data()
+else:
+    print("ERROR: These countries have no match in the csv file, please update csv file firstly and run again: ")
+    for unmatch in unmatched_countries:
+        print(unmatch)
+        
+print_all()

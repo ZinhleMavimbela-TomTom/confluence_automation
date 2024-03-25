cc5ddd7f0c83de94075070de001dfa0751fd0773
ZinhleMavimbela-TomTom <99164103+ZinhleMavimbela-TomTom@users.noreply.github.com>
2024-03-25 10:52:50 +0800

    Update push_html_data.py

--- push_html_data.py
+++ push_html_data.py
@@ -131,8 +131,9 @@ def pulling_data():
                     sanitize = u''.join(li.findAll(text=True))
                     sanitize = re.sub(r"[\n\t]*", "", sanitize)
                     sanitize = re.sub(' +', " ", sanitize)
-                    sanitize =re.sub('\xa0',"",sanitize)
-                    sanitize =re.sub('&nbsp;',"",sanitize)
+                    sanitize =re.sub('\xa0',"", sanitize)
+                    sanitize =re.sub('&nbsp;',"", sanitize)
+                    sanitize = re.sub('&amp;', "and", sanitize)
                     if ((len(remove_keyword))!=0):
                         split_remove =remove_keyword.split(",")
                         for i in range(len(split_remove)):
